fc7e88df8529734cfef571c84c4ff26dae998666
Leo Hui <leohxj@gmail.com>
2018-01-08 19:33:45 +0800

    feat: add commitlint

--- .cz-config.js
+++ .cz-config.js
@@ -4,52 +4,45 @@ module.exports = {
 
   types: [
     {
-      value: '💪  WIP',
-      name : '💪  WIP: Work in progress'
+      value: 'WIP',
+      name : '💪  WIP:      Work in progress'
     },
     {
-      value: '✨  feat',
-      name : '✨  Feature: A new feature'
+      value: 'feat',
+      name : '✨  feat:     A new feature'
     },
     {
-      value: '🐞  fix',
-      name : '🐞  Bug Fixes: A bug fix'
+      value: 'fix',
+      name : '🐞  fix:      A bug fix'
     },
     {
-      value: '✏️  chore',
-      name : '✏️  Chores: Other changes that don\'t modify src or test files'
+      value: 'refactor',
+      name : '🛠  refactor: A code change that neither fixes a bug nor adds a feature'
     },
     {
-      value: '🎨  style',
-      name : '🎨  Style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)'
+      value: 'docs',
+      name : '📚  docs:     Documentation only changes'
     },
     {
-      value: '📚  docs',
-      name : '📚  Documentation: Documentation only changes'
+      value: 'test',
+      name : '🏁  test:     Add missing tests or correcting existing tests'
     },
     {
-      value: '🛠  refactor',
-      name : '🛠  Code Refactoring: A code change that neither fixes a bug nor adds a feature'
+      value: 'chore',
+      name : '🗯  chore:    Changes that don\'t modify src or test files. Such as updating build tasks, package manager'
     },
     {
-      value: '🚀  perf',
-      name : '🚀  Performance Improvements: A code change that improves performance'
+      value: 'style',
+      name : '💅  style:    Code Style, Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)'
     },
     {
-      value: '🏁  test',
-      name : '🏁  Tests: Adding missing tests or correcting existing tests'
-    },
-    {
-      value: '🏗️  build',
-      name : '🏗️  Builds: Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)'
-    },
-    {
-      value: '⏪  revert',
-      name : '⏪  revert: Revert to a commit'
+      value: 'revert',
+      name : '⏪  revert:   Revert to a commit'
     }
   ],
 
   scopes: [],
 
-  allowCustomScopes: true
+  allowCustomScopes: true,
+  allowBreakingChanges: ["feat", "fix"]
 };
